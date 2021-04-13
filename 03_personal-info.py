3187e4472ac0dfbdd77fc0175f47b7d4abc2cdb4
VeniceTorcuator <82429740+VeniceTorcuator@users.noreply.github.com>
2021-04-13 13:18:06 +0800

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,10 @@
+print("Personal Information\n")
+
+Firstname = ("Venice")
+Lastname =("Torcuator")
+age = ("19 yrs.old")
+location = ("Bacoor Cavite")
+
+print(Firstname, Lastname, location, age)
+
+input("\nPress any key to close")
